89eb13e5b4e4e15027ea090e26f03f9ed11bc668
Braeden Ford <bford0002@gmail.com>
2022-10-18 10:52:00 -0500

    buttons working/correct math

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -14,6 +14,11 @@ const buttonValues = [
   [0, '.', '=']
 ];
 
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
 function App() {
 
   const [calc, setCalc] = useState({
@@ -23,81 +28,86 @@ function App() {
   });
 
   const numClickHandler = (e) => {
-    e.preventDefault();
     const value = e.target.innerHTML;
 
-    if (calc.num.length < 16) {
+    if (removeSpaces(calc.num).length < 16) {
       setCalc({
         ...calc,
         num:
           calc.num === 0 && value === "0"
             ? "0"
-            : calc.num % 1 === 0
-            ? Number(calc.num + value)
-            : calc.num + value,
+            : removeSpaces(calc.num) % 1 === 0
+            ? toLocaleString(Number(removeSpaces(calc.num + value)))
+            : toLocaleString(calc.num + value),
         res: !calc.sign ? 0 : calc.res,
       });
     }
   };
 
   const commaClickHandler = (e) => {
-    e.prefentDefault();
+    e.preventDefault();
     const value = e.target.innerHTML;
 
     setCalc({
       ...calc,
-      num: !calc.num.toString().includes('.') ? calc.num + value : calc.num,
-    })
-  }
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
 
   const signClickHandler = (e) => {
-    e.prefentDefault();
+    e.preventDefault();
     const value = e.target.innerHTML;
 
     setCalc({
       ...calc,
       sign: value,
       res: !calc.res && calc.num ? calc.num : calc.res,
-      num: 0
-    })
-  }
+      num: 0,
+    });
+  };
 
-  const equalsClickHandler = (e) => {
-    if (calc.sign && calc.res) {
+  const equalsClickHandler = () => {
+    if (calc.sign && calc.num) {
       const math = (a, b, sign) =>
-        sign === '+'
-        ? a + b
-        : sign === '-'
-        ? a - b
-        : sign === 'X'
-        ? a * b
-        : a / b;
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "X"
+          ? a * b
+          : a / b;
 
       setCalc({
         ...calc,
         res:
-          calc.num === '0' && calc.sign === '/'
+          calc.num === "0" && calc.sign === "/"
             ? "Can't divide with 0"
-            : math(Number(calc.res), Number(calc.num), calc.sign),
-        sign: '',
-        num: 0
+            : toLocaleString(
+                math(
+                  Number(removeSpaces(calc.res)),
+                  Number(removeSpaces(calc.num)),
+                  calc.sign
+                )
+              ),
+        sign: "",
+        num: 0,
       });
     }
-  }
+  };
 
   const invertClickHandler = () => {
     setCalc({
       ...calc,
-      num: calc.num ? calc.num * -1 : 0,
-      res: calc.res ? calc.res * -1 : 0,
+      num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
+      res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
       sign: "",
     });
   };
 
   const percentClickHandler = () => {
-    let num = calc.num ? parseFloat(calc.num) : 0;
-    let res = calc.res ? parseFloat(calc.res) : 0;
-  
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+
     setCalc({
       ...calc,
       num: (num /= Math.pow(100, 1)),
@@ -115,7 +125,6 @@ function App() {
     });
   };
 
-  console.log(calc);
 
   return (
     <Wrapper>
@@ -126,7 +135,7 @@ function App() {
             return (
               <Button
               key={i}
-              className={btn === '=' ? 'equals' : 'button'}
+              className={btn === '=' ? 'equals' : ''}
               value={btn}
               onClick={
                 btn === 'C'
